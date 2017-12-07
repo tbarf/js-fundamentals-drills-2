b55127a01e17003ee346c30566ec151db7170dfd
Mellanie Soriano <msoriano808@gmail.com>
2017-12-06 16:53:55 -1000

    first 10 exercises created

--- exercises.js
+++ exercises.js
@@ -1,11 +1,119 @@
-function doubleArray(arr) {
-  var result = [];
-  for (var i = 0; i < arr.length; i++) {
-    result.push(arr[i] * 2);
-  }
-  return result;
-}
+/* #getAllUsernames
+ *
+ * Takes in an object and returns an array of all usernames.
+ *
+ * @param {Object}
+ * @return {Array}
+ */
+
+var getAllUsernames;
+
+/* #hometownCity
+ *
+ * Takes in an array and returns a string of the users hometown city.
+ *
+ * @param {Array}
+ * @return {String}
+ */
+
+var hometownCity;
+
+/* #usersCurrentState
+ *
+ * Takes 2 arguments 'data' and 'usernames' and returns a new object with the username as the key and the user's current state as the value.
+ *
+ * @param {Array}
+ * @param {Array}
+ * @return {Object}
+ */
+
+var usersCurrentState;
+
+/* #findAdmin
+ *
+ * Takes in an object and returns the username of the admin.
+ *
+ * @param {Object}
+ * @return {String}
+ */
+
+var findAdmin;
+
+/* #addNewMovie
+ *
+ * Takes in 3 arguments 'data', 'id', 'newMovie'. Returns an array of movie titles.
+ *
+ * @param {Object}
+ * @param {Number} id
+ * @param {String} movie to add to array
+ * @return {Array}
+ */
+
+var addNewMovie;
+
+/* #favoriteBooks
+ *
+ * Takes in an object and returns an array containing an object with the users favorite books with the author as the key and the title as the value.
+ *
+ * @param {Object}
+ * @return {Array}
+ */
+
+var favoriteBooks;
+
+/* #countTracks
+ *
+ * Takes in an object and returns the number amount of tracks offered.
+ *
+ * @param {Object}
+ * @return {Number}
+ */
+
+var countTracks;
+
+/* #fullTimeStatus
+ *
+ * Takes in 2 arguments 'data' and 'trackName' and changes the selected track full time status to true.
+ *
+ * @param {Object}
+ * @param {String}
+ * @return {Object}
+ */
+
+var fullTimeStatus;
+
+/* #newTrack
+ *
+ * Takes in 3 arguments 'data', 'array', and 'string'. Returns an object with a new track with the 'string' as the key and the 'array' as the value.
+ *
+ * @param {Object}
+ * @param {Array}
+ * @param {String}
+ * @return {Object}
+ */
+
+var newTrack;
+
+/* #studentCount
+ *
+ * Takes in 2 arguments 'data' and 'trackName' and changes the selected track full time status to true.
+ *
+ * @param {Object}
+ * @param {String}
+ * @return {Object}
+ */
+
+var studentCount;
 
 module.exports = {
-  doubleArray: doubleArray
+  getAllUsernames: null,
+  hometownCity: null,
+  usersCurrentState: null,
+  findAdmin: null,
+  addNewMovie: null,
+  favoriteBooks: null,
+  countTracks: null,
+  newTrack: null,
+  fullTimeStatus: null,
+  studentCount: null
 };
